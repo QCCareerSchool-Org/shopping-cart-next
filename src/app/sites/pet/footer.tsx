94ae93a4da38e0aa783fff4f3ec1c9e20c74404c
Dave Welsh <dave@qccareerschool.com>
2023-11-13 16:25:33 -0500

    Fixed pet footer

--- src/app/sites/pet/footer.tsx
+++ src/app/sites/pet/footer.tsx
@@ -2,17 +2,24 @@
 
 import type { FC } from 'react';
 
+import styles from './footer.module.scss';
 import { useAddressState } from '@/hooks/useAddressState';
+import { gbpCountry } from '@/lib/currencies';
 import { telephoneNumber } from '@/lib/telephone';
 
 export const Footer: FC = () => {
   const { countryCode } = useAddressState();
   const tel = telephoneNumber(countryCode);
+
+  const termsLink = gbpCountry(countryCode) ? 'https://www.qcpetsudies.com/terms-gb' : 'https://www.qcpetsudies.com/terms';
+
   return (
-    <footer className="bg-black text-white">
+    <footer className={styles.footer}>
       <div className="container">
-        &copy;{new Date().getFullYear()} QC Pet Studies
-        <a href={`tel:${tel}`}>{tel}</a>
+        <span className={styles.footerItem}>&copy; {new Date().getFullYear()} QC Pet Studies</span>
+        <span className={styles.footerItem}><a target="_blank" rel="noopener noreferrer" href={termsLink}>Privacy Policy</a></span>
+        <span className={styles.footerItem}><a href={`tel:${tel}`}>{tel}</a></span>
+        <span className={styles.footerItem}><a target="_blank" rel="noreferrer" href="https://www.bbb.org/ca/on/ottawa/profile/correspondence-schools/qc-career-school-0117-4175">BBB Accredited A+</a></span>
       </div>
     </footer>
   );
