6b7a0ad3e0973d583385854cbdb927f3bc07098e
Dave Welsh <dave@qccareerschool.com>
2023-11-14 08:42:14 -0500

    Added dynamic message

--- src/app/sites/design/_default/fallback/index.tsx
+++ src/app/sites/design/_default/fallback/index.tsx
@@ -4,6 +4,7 @@ import { agreementLinks } from '../../agreementLinks';
 import { courseGroups } from '../../courseGroups';
 import { Guarantee } from '../../guarantee';
 import { DesignFallbackPromo } from './promo';
+import { Save50CourseMessage } from '@/components/dynamicCourseMessages/save50';
 import { Form } from '@/components/form';
 
 type Props = {
@@ -23,6 +24,7 @@ export const DesignFallback: FC<Props> = ({ date }) => {
         agreementLinks={agreementLinks}
         dynamicCourseDescriptions="SHOW"
         visualPaymentPlans
+        dynamicCourseMessages={[ Save50CourseMessage ]}
       />
     </>
   );
