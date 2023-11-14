d13f84c30d7f9ea44ab07f76b67069e3658b6cc9
Dave Welsh <dave@qccareerschool.com>
2023-11-14 09:35:20 -0500

    Changed sve 50 message to keep displaying as long as a course is selected

--- src/components/dynamicCourseMessages/save50.tsx
+++ src/components/dynamicCourseMessages/save50.tsx
@@ -8,7 +8,7 @@ import { useCoursesState } from '@/hooks/useCoursesState';
 export const Save50CourseMessage: FC = () => {
   const coursesState = useCoursesState();
   const courseCount = coursesState.selected.length;
-  if (courseCount === 1) {
+  if (courseCount >= 1) {
     return (
       <DynamicCourseAlert variant="info"><strong>Save 50%</strong> on each additional course of equal or lesser value</DynamicCourseAlert>
     );
