8127d65e607540d8ad88946403c8ccf718c083f4
Dave Welsh <dave@qccareerschool.com>
2023-12-05 17:30:25 -0500

    Include preview

--- src/lib/getDate.ts
+++ src/lib/getDate.ts
@@ -1,7 +1,7 @@
 'use server';
 
 export const getDate = (dateParam?: string | string[]): number => {
-  if (process.env.VERCEL_ENV === 'development' && typeof dateParam === 'string') {
+  if ((process.env.VERCEL_ENV === 'development' || process.env.VERCEL_ENV === 'preview') && typeof dateParam === 'string') {
     const override = new Date(dateParam).getTime();
     if (!isNaN(override)) {
       return override;
