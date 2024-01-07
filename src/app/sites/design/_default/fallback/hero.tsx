297e0dca6c0fe417dd24bbe654cbc05e90e855b9
Dave Welsh <dave@qccareerschool.com>
2024-01-06 19:22:31 -0500

    Updated fallback carts for makeup and design

--- src/app/sites/design/_default/fallback/hero.tsx
+++ src/app/sites/design/_default/fallback/hero.tsx
@@ -1,9 +1,20 @@
+'use client';
+
 import type { FC } from 'react';
 
+import DesktopUK from './desktop-uk.jpg';
 import Desktop from './desktop.jpg';
+import MobileUK from './mobile-uk.jpg';
 import Mobile from './mobile.jpg';
 import { PromoImage } from '@/components/promoImage';
+import { usePriceState } from '@/hooks/usePriceState';
 
 export const Hero: FC = () => {
-  return <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} />;
+  const priceState = usePriceState();
+
+  const [ desktopSrc, mobileSrc ] = priceState?.currency.code === 'GBP'
+    ? [ Desktop, Mobile ]
+    : [ DesktopUK, MobileUK ];
+
+  return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
 };
--- src/app/sites/design/_default/fallback/modal.tsx
+++ src/app/sites/design/_default/fallback/modal.tsx
@@ -1,8 +1,6 @@
 import type { FC } from 'react';
 import { Modal } from 'react-bootstrap';
 
-import { CareerEssentials } from '@/components/careerEssentials';
-
 type Props = {
   show: boolean;
   onHide: () => void;
@@ -10,11 +8,9 @@ type Props = {
 
 export const DesignFallbackModal: FC<Props> = props => (
   <Modal show={props.show} onHide={props.onHide}>
-    <Modal.Header closeButton>Course Bonuses Included</Modal.Header>
+    <Modal.Header closeButton>Gain Specialized Design Training</Modal.Header>
     <Modal.Body>
-      <p className="lead">Career Essentials Collection</p>
-      <p>Enroll in any design course to receive free design software as well as our brand new Career Essentials Collection filled with business &amp; social media templates!</p>
-      <CareerEssentials />
+      <p className="lead mb-0">For a limited time, enroll in any design course and get Virtual Design FREE!</p>
     </Modal.Body>
   </Modal>
 );
