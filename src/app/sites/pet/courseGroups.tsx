85b3fa2bdf7fb4b384fad7826a52c2caa82e2b41
Dave Welsh <dave@qccareerschool.com>
2024-01-29 13:37:44 -0500

    Don't hide DC

--- src/app/sites/pet/courseGroups.tsx
+++ src/app/sites/pet/courseGroups.tsx
@@ -6,7 +6,15 @@ export const courseGroups: CourseGroup[] = [
     items: [
       { code: 'DG', name: 'Dog Grooming', badge: <DogGroomingKitTag /> },
       { code: 'DT', name: 'Dog Training' },
-      { code: 'DC', name: 'Dog Behavior' },
+      {
+        code: 'DC',
+        name: 'Dog Behavior',
+        disabledMessage: (
+          <>
+            The <span className="text-primary">Dog Behavior</span> course is designed to be taken after the <span className="text-primary">Dog Training</span> course. To enroll in Dog Behavior, please select Dog Training first.
+          </>
+        ),
+      },
       { code: 'DD', name: 'Dog Daycare' },
     ],
   },
