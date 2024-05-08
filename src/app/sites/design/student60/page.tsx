a813c1669f926d3568f1fd8479916d64bf5e60f4
Dave Welsh <dave@qccareerschool.com>
2024-05-08 16:37:08 -0400

    Added event and design save 60 pages

---
+++ src/app/sites/design/student60/page.tsx
@@ -0,0 +1,31 @@
+import { agreementLinks } from '../agreementLinks';
+import { courseGroups } from '../courseGroups';
+import { Guarantee } from '../guarantee';
+import { DesignStudent60Promo } from './promo';
+import { Form } from '@/components/form';
+import { getDate } from '@/lib/getDate';
+import type { PageComponent } from '@/serverComponent';
+
+const DesignSave60Page: PageComponent = ({ searchParams }) => {
+  const date = getDate(searchParams.date);
+
+  return (
+    <>
+      <DesignStudent60Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Design School"
+        guarantee={Guarantee}
+        successLink="https://www.qcdesignschool.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        student
+        dynamicCourseDescriptions="HIDE"
+        promoCodeDefault="SAVE60"
+        visualPaymentPlans
+      />
+    </>
+  );
+};
+
+export default DesignSave60Page;
---
+++ src/app/sites/event/student60/promo.tsx
@@ -0,0 +1,27 @@
+import type { FC } from 'react';
+
+import { Hero } from './hero';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+
+const backgroundColor = '#aaaba9';
+const endDate = Date.UTC(2024, 4, 10, 10); // May 10 6:00PM ET (10:00 UTC)
+
+type Props = {
+  date: number;
+};
+
+export const EventStudent60Promo: FC<Props> = ({ date }) => (
+  <>
+    <Section style={{ backgroundColor }} noPadding>
+      <Hero />
+    </Section>
+    <CountDownTimerWrapper
+      date={date}
+      showDate={date}
+      endDate={endDate}
+      message={<span style={{ textTransform: 'uppercase' }}><strong>LAST CHANCE</strong> This exclusive ends soon!</span>}
+      className="bg-primary text-light"
+    />
+  </>
+);
