56eb4bc5ef494c49a05b376e2761dd3ce7f65752
Dave Welsh <dave@qccareerschool.com>
2024-05-23 10:15:28 -0400

    Replaced hero graphics

--- src/app/sites/event/_carts/2024/05/22/hero.tsx
+++ src/app/sites/event/_carts/2024/05/22/hero.tsx
@@ -1,28 +1,19 @@
-'use client';
-
 import type { FC } from 'react';
 
 import DesktopEnds from './desktop-ends.jpg';
-import DesktopUK from './desktop-uk.jpg';
 import Desktop from './desktop.jpg';
 import MobileEnds from './mobile-ends.jpg';
-import MobileUK from './mobile-uk.jpg';
 import Mobile from './mobile.jpg';
 import { PromoImage } from '@/components/promoImage';
-import { usePriceState } from '@/hooks/usePriceState';
 
 type Props = {
   lastChance: boolean;
 };
 
 export const Hero: FC<Props> = ({ lastChance }) => {
-  const price = usePriceState();
-
   const [ desktopSrc, mobileSrc ] = lastChance
     ? [ DesktopEnds, MobileEnds ]
-    : price?.currency.code === 'GBP'
-      ? [ Desktop, Mobile ]
-      : [ DesktopUK, MobileUK ];
+    : [ Desktop, Mobile ];
 
   return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
 };
