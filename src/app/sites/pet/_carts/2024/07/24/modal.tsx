53059ff86be0256d6dd4f5edc50831489e47b761
Dave Welsh <dave@qccareerschool.com>
2024-07-24 12:43:36 -0400

    Changed font of headings in popup

--- src/app/sites/pet/_carts/2024/07/24/modal.tsx
+++ src/app/sites/pet/_carts/2024/07/24/modal.tsx
@@ -19,12 +19,13 @@ export const Pet20240724Modal: FC<Props> = props => {
     <Modal show={props.show} onHide={props.onHide}>
       <Modal.Header closeButton>Start Your Pet Care Career</Modal.Header>
       <Modal.Body>
-        <p className="lead">Enroll in any pet course and get {discount} off your tuition! Choose from in-demand professions like grooming, training, or dog daycare services.</p>
-        <h6>Learn From Expert Instructors</h6>
+        <p>Enroll in any pet course and get {discount} off your tuition! Choose from in-demand professions like grooming, training, or dog daycare services.</p>
+        <hr />
+        <h6 className="sans-serif">Learn From Expert Instructors</h6>
         <p>We've hand-selected the top pet care professionals in the industry to guide you through your course material.</p>
-        <h6>Industry-Recognized Certifications</h6>
+        <h6 className="sans-serif">Industry-Recognized Certifications</h6>
         <p>Graduate with professional pet industry certifications and open up a world of career & business opportunities.</p>
-        <h6>Business Training Included</h6>
+        <h6 className="sans-serif">Business Training Included</h6>
         <p className="mb-0">Each course includes built-in business training to help you launch your career and grow your clientele as a newly certified trainer, groomer or daycare owner</p>
       </Modal.Body>
     </Modal>
