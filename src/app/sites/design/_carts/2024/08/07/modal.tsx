dddff9ac7bca311273b36e83aa5405ee8038e85f
Dave Welsh <dave@qccareerschool.com>
2024-08-07 11:44:57 -0400

    Updated popup text

--- src/app/sites/design/_carts/2024/08/07/modal.tsx
+++ src/app/sites/design/_carts/2024/08/07/modal.tsx
@@ -2,6 +2,7 @@
 
 import type { FC } from 'react';
 import { Modal } from 'react-bootstrap';
+import { usePriceState } from '@/hooks/usePriceState';
 
 type Props = {
   show: boolean;
@@ -9,11 +10,14 @@ type Props = {
 };
 
 export const Design20240807Modal: FC<Props> = props => {
+  const priceState = usePriceState();
+  const discount = priceState?.currency.code === 'GBP' ? '£100' : '$100';
+
   return (
     <Modal show={props.show} onHide={props.onHide}>
       <Modal.Header closeButton>Start Your Design Career</Modal.Header>
       <Modal.Body>
-        <p>For a limited time, enroll in any design course and <strong>get a second course free</strong>. This is the perfect opportunity to specialize in your design training and become a certified expert.</p>
+        <p>For a limited time, enroll in any design course and you'll <strong>get {discount} off and a second course free</strong>. This is the perfect opportunity to specialize in your design training and become a certified expert.</p>
         <hr />
         <h6 className="sans-serif">Learn From Expert Instructors</h6>
         <p>We've hand-selected the top home design professionals in the industry to guide you through your course material.</p>
