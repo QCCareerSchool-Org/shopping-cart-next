2551704aa73614fd0da8dcec4ebb343972cd9d56
Dave Welsh <dave@qccareerschool.com>
2024-09-06 16:34:09 -0400

    Changed to 100OFF promotion

--- src/app/sites/wellness/_carts/fallback/hero.tsx
+++ src/app/sites/wellness/_carts/fallback/hero.tsx
@@ -2,19 +2,28 @@
 
 import type { FC } from 'react';
 
+import DesktopEnds from './desktop-ends.jpg';
+import DesktopUKEnds from './desktop-uk-ends.jpg';
 import DesktopUK from './desktop-uk.jpg';
 import Desktop from './desktop.jpg';
+import MobileEnds from './mobile-ends.jpg';
+import MobileUKEnds from './mobile-uk-ends.jpg';
 import MobileUK from './mobile-uk.jpg';
 import Mobile from './mobile.jpg';
+
 import { PromoImage } from '@/components/promoImage';
 import { usePriceState } from '@/hooks/usePriceState';
 
-export const Hero: FC = () => {
+type Props = {
+  lastChance?: boolean;
+};
+
+export const Hero: FC<Props> = ({ lastChance }) => {
   const priceState = usePriceState();
 
-  const [ desktopSrc, mobileSrc ] = priceState?.currency.code === 'GBP'
-    ? [ DesktopUK, MobileUK ]
-    : [ Desktop, Mobile ];
+  const [ desktopSrc, mobileSrc ] = lastChance
+    ? priceState?.currency.code === 'GBP' ? [ DesktopUKEnds, MobileUKEnds ] : [ DesktopEnds, MobileEnds ]
+    : priceState?.currency.code === 'GBP' ? [ DesktopUK, MobileUK ] : [ Desktop, Mobile ];
 
   return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
 };
