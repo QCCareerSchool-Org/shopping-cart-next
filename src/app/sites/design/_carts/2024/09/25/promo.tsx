eb5a546b4c6825049d4b93789675eded7a1497b7
Dave Welsh <dave@qccareerschool.com>
2024-09-20 09:03:28 -0400

    Added promo pages

---
+++ src/app/sites/design/_carts/2024/09/25/promo.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import type { FC } from 'react';
+
+import { Hero } from './hero';
+import { Design20240925Modal } from './modal';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+import { useToggle } from '@/hooks/useToggle';
+
+const backgroundColor = '#223d4d';
+const lastChanceDate = Date.UTC(2024, 8, 28, 4); // 2024-09-28T00:00 (04:00 UTC)
+const endDate = Date.UTC(2024, 9, 2, 4); // 2024-10-02T00:00 (04:00 UTC)
+
+type Props = {
+  date: number;
+};
+
+export const Design20240925Promo: FC<Props> = ({ date }) => {
+  const [ showPopup, togglePopup ] = useToggle(false);
+
+  const handleClick = (): void => {
+    togglePopup();
+  };
+
+  return (
+    <>
+      <Section style={{ backgroundColor }} noPadding>
+        <div onClick={handleClick} style={{ cursor: 'pointer' }}>
+          <Hero lastChance={date >= lastChanceDate} />
+        </div>
+      </Section>
+      <Design20240925Modal show={showPopup} onHide={handleClick} />
+      <CountDownTimerWrapper
+        date={date}
+        showDate={lastChanceDate}
+        endDate={endDate}
+        message={<span style={{ textTransform: 'uppercase' }}><strong>LAST CHANCE</strong> This exclusive offer ends soon!</span>}
+        className="bg-black text-light"
+      />
+    </>
+  );
+};
