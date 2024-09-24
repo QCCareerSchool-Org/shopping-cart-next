019685d96d96b05e28b1abbd8340453daaa2c2c8
Dave Welsh <dave@qccareerschool.com>
2024-09-24 13:21:23 -0400

    Added student promotions for Sept 25

---
+++ src/app/sites/makeup/student/_carts/2024/09/25/hero.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+import Desktop from './desktop.jpg';
+import Mobile from './mobile.jpg';
+import { PromoImage } from '@/components/promoImage';
+
+export const Hero: FC = () => (
+  <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} />
+);
---
+++ src/app/sites/makeup/student/_carts/2024/09/25/index.tsx
@@ -0,0 +1,31 @@
+import type { FC } from 'react';
+
+import { agreementLinks } from '../../../../../agreementLinks';
+import { courseGroups } from '../../../../../courseGroups';
+import { Guarantee } from '../../../../../guarantee';
+import { MakeupStudent20240925Promo } from './promo';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const MakeupStudent20240925: FC<Props> = ({ date }) => {
+  return (
+    <>
+      <MakeupStudent20240925Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Makeup Academy"
+        guarantee={Guarantee}
+        successLink="https://www.qcmakeupacademy.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        student
+        dynamicCourseDescriptions="SHOW"
+        visualPaymentPlans
+        promoCodeDefault="BRUSHSET50"
+      />
+    </>
+  );
+};
