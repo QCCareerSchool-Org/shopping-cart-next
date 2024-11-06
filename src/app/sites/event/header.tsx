0eacd23adbf4a8b2030db624f8fc1315d23a4d7e
Dave Welsh <dave@qccareerschool.com>
2024-11-06 14:10:08 -0500

    Switch back to regular luminous kit

--- src/app/sites/event/header.tsx
+++ src/app/sites/event/header.tsx
@@ -9,17 +9,17 @@ import Logo from './logo.svg';
 import { useAddressState } from '@/hooks/useAddressState';
 import { telephoneNumber } from '@/lib/telephone';
 
-const backgroundColor = '#252525';
+const backgroundColor = 'white';
 
 export const Header: FC = () => {
   const { countryCode } = useAddressState();
   const tel = telephoneNumber(countryCode);
   return (
-    <header className="text-white" style={{ backgroundColor }}>
+    <header style={{ backgroundColor }}>
       <div className="container py-3 py-sm-4">
         <div className="row">
-          <div className="col-9 col-sm-12 text-start text-sm-center"><a href="https://www.qceventplanning.com/"><Image src={Logo as StaticImageData} priority className="img-fluid" alt="QC Makeup Academy" style={{ height: 32 }} /></a></div>
-          <div className="col-3 d-sm-none text-end"><a title="Click to Call" href={'tel:' + tel}><FaPhone size={32} /></a></div>
+          <div className="col-9 col-sm-12 text-start text-sm-center"><a href="https://www.qceventplanning.com/"><Image src={Logo as StaticImageData} priority className="img-fluid" alt="QC Event School" style={{ height: 30 }} /></a></div>
+          <div className="col-3 d-sm-none text-end"><a title="Click to Call" href={'tel:' + tel}><FaPhone size={30} /></a></div>
         </div>
       </div>
     </header>
--- src/app/sites/makeup/_carts/2024/11/06/modal.tsx
+++ src/app/sites/makeup/_carts/2024/11/06/modal.tsx
@@ -3,7 +3,7 @@
 import type { FC } from 'react';
 import { Modal } from 'react-bootstrap';
 
-import { LuminousPlusSFXKit } from '@/components/luminousPlusSFXKit';
+import { LuminousKit } from '@/components/luminousKit';
 
 type Props = {
   show: boolean;
@@ -15,7 +15,7 @@ export const Makeup20241106Modal: FC<Props> = props => (
     <Modal.Header closeButton>Early Black Friday Offer</Modal.Header>
     <Modal.Body>
       <p>For a limited time, enroll in <strong>Master Makeup Artistry</strong> and get ANY second course FREE of equal or lesser value. You'll also get the <strong>ENTIRE Luminous Makeup Collection</strong>.</p>
-      <LuminousPlusSFXKit />
+      <LuminousKit />
     </Modal.Body>
   </Modal>
 );
