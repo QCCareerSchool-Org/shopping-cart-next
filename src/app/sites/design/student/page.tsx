972fd5009b61d719e26869740a83cb9a6aa3d251
Dave Welsh <dave@qccareerschool.com>
2024-11-15 16:20:32 -0500

    Started student promos

--- src/app/sites/design/student/page.tsx
+++ src/app/sites/design/student/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { DesignStudent20241106 } from './_carts/2024/11/06';
+import { DesignStudent20241115 } from './_carts/2024/11/15';
 import { DesignStudentFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -10,8 +10,9 @@ const DesignStudentPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
+      {/* {date < Date.UTC(2024, 10, 30, 8) // November 30, 2024 at 03:00 (08:00 UTC) */}
       {date >= Date.UTC(2024, 10, 6, 13) && date < Date.UTC(2024, 10, 13, 5) // November 6, 2024 at 08:00 (13:00 UTC) to Nov 13, 2024 at 00:00 (05:00 UTC)
-        ? <DesignStudent20241106 date={date} />
+        ? <DesignStudent20241115 date={date} />
         : <DesignStudentFallback date={date} />
       }
     </Suspense>
---
+++ src/app/sites/event/student/_carts/2024/11/15/index.tsx
@@ -0,0 +1,31 @@
+import type { FC } from 'react';
+
+import { EventStudent20241115Promo } from './promo';
+import { agreementLinks } from '../../../../../agreementLinks';
+import { courseGroups } from '../../../../../courseGroups';
+import { Guarantee } from '../../../../../guarantee';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const EventStudent20241115: FC<Props> = ({ date }) => {
+  return (
+    <>
+      <EventStudent20241115Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Event School"
+        guarantee={Guarantee}
+        successLink="https://www.qceventplanning.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        student
+        dynamicCourseDescriptions="HIDE"
+        visualPaymentPlans
+        promoCodeDefault="PORTFOLIO"
+      />
+    </>
+  );
+};
