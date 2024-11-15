dcfdbf0a99789e116361a5ca3935a6c83f04f29e
Dave Welsh <dave@qccareerschool.com>
2024-11-15 16:25:31 -0500

    Fixed promo code

--- src/app/sites/event/_carts/2024/11/15/index.tsx
+++ src/app/sites/event/_carts/2024/11/15/index.tsx
@@ -23,7 +23,7 @@ export const Event20241115: FC<Props> = ({ date }) => (
       agreementLinks={agreementLinks}
       dynamicCourseDescriptions="HIDE"
       visualPaymentPlans
-      promoCodeDefault="2FREESPECIALTY"
+      promoCodeDefault="2SPECIALTY"
       dynamicCourseMessages={[ TwoFreeEventSpecialtyDynamicMessage ]}
     />
   </>
