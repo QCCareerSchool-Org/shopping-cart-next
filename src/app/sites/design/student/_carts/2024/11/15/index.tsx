46b71a04266330665b2d2f8b70bfe1d78e2cbb8d
Dave Welsh <dave@qccareerschool.com>
2024-11-22 15:29:09 -0500

    Added promo code change

--- src/app/sites/design/student/_carts/2024/11/15/index.tsx
+++ src/app/sites/design/student/_carts/2024/11/15/index.tsx
@@ -24,7 +24,7 @@ export const DesignStudent20241115: FC<Props> = ({ date }) => {
         student
         dynamicCourseDescriptions="SHOW"
         visualPaymentPlans
-        promoCodeDefault="COLORWHEEL"
+        promoCodeDefault="COLORWHEEL60"
       />
     </>
   );
