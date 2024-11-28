01564b8f6e8ccbe56554c8ce35d5473366522fbb
Dave Welsh <dave@qccareerschool.com>
2024-11-28 14:33:50 -0500

    Added Cyber Monday promotions and extended Black Friday promotions

---
+++ src/app/sites/design/_carts/2024/12/01/index.tsx
@@ -0,0 +1,32 @@
+import type { FC } from 'react';
+
+import { Design20241201Promo } from './promo';
+import { agreementLinks } from '@/app/sites/design/agreementLinks';
+import { courseGroups } from '@/app/sites/design/courseGroups';
+import { Guarantee } from '@/app/sites/design/guarantee';
+import { BogoDynamicCourseMessage } from '@/components/dynamicCourseMessages/bogo';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const Design20241201: FC<Props> = ({ date }) => {
+  return (
+    <>
+      <Design20241201Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Design School"
+        guarantee={Guarantee}
+        successLink="https://www.qcdesignschool.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        dynamicCourseDescriptions="SHOW"
+        visualPaymentPlans
+        promoCodeDefault="BOGOCATALYST100"
+        dynamicCourseMessages={[ BogoDynamicCourseMessage ]}
+      />
+    </>
+  );
+};
---
+++ src/app/sites/makeup/_carts/2024/12/01/modal.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import type { FC } from 'react';
+import { Modal } from 'react-bootstrap';
+
+import { LuminousKit } from '@/components/luminousKit';
+
+type Props = {
+  show: boolean;
+  onHide: () => void;
+};
+
+export const Makeup20241201Modal: FC<Props> = props => (
+  <Modal show={props.show} onHide={props.onHide}>
+    <Modal.Header closeButton>Cyber Monday Special</Modal.Header>
+    <Modal.Body>
+      <p>For a limited time, enroll in <strong>Master Makeup Artistry</strong> and get Pro Makeup Workshop FREE. You'll also get the ENTIRE Luminous Makeup Collection.</p>
+      <LuminousKit />
+    </Modal.Body>
+  </Modal>
+);
