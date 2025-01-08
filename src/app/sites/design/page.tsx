2fb69c13c5cb14f4e08ea0974b9ac6c93b0d4b96
Dave Welsh <dave@qccareerschool.com>
2025-01-08 14:15:36 -0500

    Added Jan 10 promotions

--- src/app/sites/design/page.tsx
+++ src/app/sites/design/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 
-import { Design20241213 } from './_carts/2024/12/13';
-import { Design20241226 } from './_carts/2024/12/26';
+import { Design20250110 } from './_carts/2025/01/10';
 import { DesignFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -11,11 +10,9 @@ const DesignPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date < Date.UTC(2024, 11, 19, 8) // 2024-12-19T03:00 (08:00 UTC)
-        ? <Design20241213 date={date} />
-        : date >= Date.UTC(2024, 11, 26, 8) && date < Date.UTC(2025, 0, 6, 8) // 2024-12-26T03:00 (08:00 UTC) to 2025-01-06T03:00 (08:00 UTC)
-          ? <Design20241226 date={date} />
-          : <DesignFallback date={date} />
+      {date >= Date.UTC(2025, 0, 10, 8) && date < Date.UTC(2025, 0, 19, 8) // 2025-01-10T03:00 (08:00 UTC) to 2025-01-19T03:00 (08:00 UTC)
+        ? <Design20250110 date={date} />
+        : <DesignFallback date={date} />
       }
     </Suspense>
   );
--- src/app/sites/wellness/page.tsx
+++ src/app/sites/wellness/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 
-import { Wellness20241213 } from './_carts/2024/12/13';
-import { Wellness20241226 } from './_carts/2024/12/26';
+import { Wellness20250110 } from './_carts/2025/01/10';
 import { WellnessFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -11,11 +10,9 @@ const DesignPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date < Date.UTC(2024, 11, 19, 8) // 2024-12-19T03:00 (08:00 UTC)
-        ? <Wellness20241213 date={date} />
-        : date >= Date.UTC(2024, 11, 26, 8) && date < Date.UTC(2025, 0, 6, 8) // 2024-12-26T03:00 (08:00 UTC) to 2025-01-06T03:00 (08:00 UTC)
-          ? <Wellness20241226 date={date} />
-          : <WellnessFallback date={date} />
+      {date >= Date.UTC(2025, 0, 10, 8) && date < Date.UTC(2025, 0, 19, 8) // 2025-01-10T03:00 (08:00 UTC) to 2025-01-19T03:00 (08:00 UTC)
+        ? <Wellness20250110 date={date} />
+        : <WellnessFallback date={date} />
       }
     </Suspense>
   );
