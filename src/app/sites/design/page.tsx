07904883341284a15dd2db825c58e7692a50cd8c
Dave Welsh <dave@qccareerschool.com>
2025-02-24 15:57:43 -0500

    Removed other promos

--- src/app/sites/design/page.tsx
+++ src/app/sites/design/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 
 import { Design20250206 } from './_carts/2025/02/06';
-import { Design20250225 } from './_carts/2025/02/25';
+import { Design20250305 } from './_carts/2025/03/05';
 import { DesignFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -13,7 +13,7 @@ const DesignPage: PageComponent = async ({ searchParams }) => {
   return (
     <Suspense>
       {date >= Date.UTC(2025, 1, 25, 8) && date < Date.UTC(2025, 2, 1, 8) // 2025-02-25T03:00 (08:00 UTC) to 2025-03-01T03:00 (08:00 UTC)
-        ? <Design20250225 date={date} />
+        ? <Design20250305 date={date} />
         : date >= Date.UTC(2025, 2, 5, 5) && date < Date.UTC(2025, 1, 11, 7) // 2025-03-05T00:00 (05:00 UTC) to 2025-03-11T03:00 (07:00 UTC)
           ? <Design20250206 date={date} />
           : <DesignFallback date={date} />
--- src/app/sites/event/page.tsx
+++ src/app/sites/event/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 
 import { Event20250206 } from './_carts/2025/02/06';
-import { Event20250225 } from './_carts/2025/02/25';
+import { Event20250305 } from './_carts/2025/03/05';
 import { EventFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -12,7 +12,7 @@ const EventPage: PageComponent = async ({ searchParams }) => {
   return (
     <Suspense>
       {date >= Date.UTC(2025, 1, 25, 8) && date < Date.UTC(2025, 2, 1, 8) // 2025-02-25T03:00 (08:00 UTC) to 2025-03-01T03:00 (08:00 UTC)
-        ? <Event20250225 date={date} />
+        ? <Event20250305 date={date} />
         : date >= Date.UTC(2025, 2, 5, 5) && date < Date.UTC(2025, 1, 11, 7) // 2025-03-05T00:00 (05:00 UTC) to 2025-03-11T03:00 (07:00 UTC)
           ? <Event20250206 date={date} />
           : <EventFallback date={date} />
