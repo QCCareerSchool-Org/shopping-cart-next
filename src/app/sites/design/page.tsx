2b0d95ea419554926af1a906de49322e702c8a58
Dave Welsh <dave@qccareerschool.com>
2025-02-24 16:17:10 -0500

    Fixed cart order

--- src/app/sites/design/page.tsx
+++ src/app/sites/design/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { Design20250206 } from './_carts/2025/02/06';
+import { Design20250225 } from './_carts/2025/02/25';
 import { Design20250305 } from './_carts/2025/03/05';
 import { DesignFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
@@ -12,9 +12,9 @@ const DesignPage: PageComponent = async ({ searchParams }) => {
   return (
     <Suspense>
       {date >= Date.UTC(2025, 1, 25, 8) && date < Date.UTC(2025, 2, 1, 8) // 2025-02-25T03:00 (08:00 UTC) to 2025-03-01T03:00 (08:00 UTC)
-        ? <Design20250305 date={date} />
+        ? <Design20250225 date={date} />
         : date >= Date.UTC(2025, 2, 5, 5) && date < Date.UTC(2025, 2, 11, 7) // 2025-03-05T00:00 (05:00 UTC) to 2025-03-11T03:00 (07:00 UTC)
-          ? <Design20250206 date={date} />
+          ? <Design20250305 date={date} />
           : <DesignFallback date={date} />
       }
     </Suspense>
--- src/app/sites/pet/page.tsx
+++ src/app/sites/pet/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { Pet20250206 } from './_carts/2025/02/06';
+import { Pet20250225 } from './_carts/2025/02/25';
 import { Pet20250305 } from './_carts/2025/03/05';
 import { PetFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
@@ -12,9 +12,9 @@ const PetPage: PageComponent = async ({ searchParams }) => {
   return (
     <Suspense>
       {date >= Date.UTC(2025, 1, 25, 8) && date < Date.UTC(2025, 2, 1, 8) // 2025-02-25T03:00 (08:00 UTC) to 2025-03-01T03:00 (08:00 UTC)
-        ? <Pet20250305 date={date} />
+        ? <Pet20250225 date={date} />
         : date >= Date.UTC(2025, 2, 5, 5) && date < Date.UTC(2025, 2, 11, 7) // 2025-03-05T00:00 (05:00 UTC) to 2025-03-11T03:00 (07:00 UTC)
-          ? <Pet20250206 date={date} />
+          ? <Pet20250305 date={date} />
           : <PetFallback date={date} />
       }
     </Suspense>
