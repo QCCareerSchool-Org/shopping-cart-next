aea11188b5240cacd3734116e960ecd354faa509
Dave Welsh <dave@qccareerschool.com>
2025-02-24 15:50:15 -0500

    Added Feb 25 promos

--- src/app/sites/design/page.tsx
+++ src/app/sites/design/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 
 import { Design20250206 } from './_carts/2025/02/06';
-import { Design20250216 } from './_carts/2025/02/16';
+import { Design20250225 } from './_carts/2025/02/25';
 import { DesignFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -9,11 +9,12 @@ import type { PageComponent } from '@/serverComponent';
 const DesignPage: PageComponent = async ({ searchParams }) => {
   const date = await getDate(searchParams.date);
 
+  //  March 5th (00:00AM) - March 11th (03:00 AM)
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 1, 16, 8) && date < Date.UTC(2025, 1, 23, 8) // 2025-02-16T03:00 (08:00 UTC) to 2025-02-23T03:00 (08:00 UTC)
-        ? <Design20250216 date={date} />
-        : date >= Date.UTC(2025, 1, 6, 8) && date < Date.UTC(2025, 1, 16, 8) // 2025-02-06T03:00 (08:00 UTC) to 2025-02-16T03:00 (08:00 UTC)
+      {date >= Date.UTC(2025, 1, 25, 8) && date < Date.UTC(2025, 2, 1, 8) // 2025-02-25T03:00 (08:00 UTC) to 2025-03-01T03:00 (08:00 UTC)
+        ? <Design20250225 date={date} />
+        : date >= Date.UTC(2025, 2, 5, 5) && date < Date.UTC(2025, 1, 11, 7) // 2025-03-05T00:00 (05:00 UTC) to 2025-03-11T03:00 (07:00 UTC)
           ? <Design20250206 date={date} />
           : <DesignFallback date={date} />
       }
---
+++ src/app/sites/event/_carts/2025/03/27/promo.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { type FC, useMemo } from 'react';
+
+import { Hero20250216 } from './hero';
+import { Event20250216Modal } from './modal';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+import { useToggle } from '@/hooks/useToggle';
+
+const backgroundColor = '#b2b3ae';
+const extensionDate = Date.UTC(2025, 1, 20, 4); // 2025-02-20T00:00 (04:00 UTC)
+
+type Props = {
+  date: number;
+};
+
+export const Event20250216Promo: FC<Props> = ({ date }) => {
+  const [ showPopup, togglePopup ] = useToggle(false);
+
+  const [ lastChanceDate, endDate ] = useMemo(() => {
+    if (date >= extensionDate) {
+      return [
+        Date.UTC(2025, 1, 21, 8), // 2025-02-21T03:00 (08:00 UTC)
+        Date.UTC(2025, 1, 22, 8), // 2025-02-22T03:00 (08:00 UTC)
+      ];
+    }
+    return [
+      Date.UTC(2025, 1, 19, 8), // 2025-02-19T03:00 (08:00 UTC)
+      Date.UTC(2025, 1, 20, 4), // 2025-02-20T00:00 (08:00 UTC)
+    ];
+  }, [ date ]);
+
+  const variant = date >= lastChanceDate ? 'lastChance' : undefined;
+
+  const handleClick = (): void => {
+    togglePopup();
+  };
+
+  return (
+    <>
+      <Section style={{ backgroundColor }} noPadding>
+        <div onClick={handleClick} style={{ cursor: 'pointer' }}>
+          <Hero20250216 variant={variant} />
+        </div>
+      </Section>
+      <Event20250216Modal show={showPopup} onHide={handleClick} />
+      <CountDownTimerWrapper
+        date={date}
+        showDate={lastChanceDate}
+        endDate={endDate}
+        message={<span style={{ textTransform: 'uppercase' }}><strong>LAST CHANCE</strong> This exclusive offer ends soon!</span>}
+        className="bg-black text-light"
+      />
+    </>
+  );
+};
