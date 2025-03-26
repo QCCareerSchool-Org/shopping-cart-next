38a907a22a5b0ccc0523577544472368f1f0ccd6
Dave Welsh <dave@qccareerschool.com>
2025-03-26 12:38:39 -0400

    Added March 27 promotions

---
+++ src/app/sites/design/_carts/2025/03/27/hero.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import type { StaticImageData } from 'next/image';
+import type { FC } from 'react';
+
+import DesktopEnds from './desktop-ends.jpg';
+import DesktopUK from './desktop-uk.jpg';
+import Desktop from './desktop.jpg';
+import MobileEnds from './mobile-ends.jpg';
+import MobileUK from './mobile-uk.jpg';
+import Mobile from './mobile.jpg';
+import { PromoImage } from '@/components/promoImage';
+import { usePriceState } from '@/hooks/usePriceState';
+
+type Props = {
+  variant?: 'lastChance';
+};
+
+export const Hero20250327: FC<Props> = ({ variant }) => {
+  const priceState = usePriceState();
+  const [ desktopSrc, mobileSrc ] = getSrcs(variant, priceState?.currency.code);
+  return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
+};
+
+const getSrcs = (variant?: 'lastChance', currencyCode?: string): [ StaticImageData, StaticImageData ] => {
+  if (variant === 'lastChance') {
+    return [ DesktopEnds, MobileEnds ];
+  }
+  return currencyCode === 'GBP' ? [ DesktopUK, MobileUK ] : [ Desktop, Mobile ];
+};
--- src/app/sites/event/page.tsx
+++ src/app/sites/event/page.tsx
@@ -1,9 +1,7 @@
 import { Suspense } from 'react';
 
-import { Event20250225 } from './_carts/2025/02/25';
-import { Event20250305 } from './_carts/2025/03/05';
-import { Event20250312 } from './_carts/2025/03/12';
 import { Event20250319 } from './_carts/2025/03/19';
+import { Event20250327 } from './_carts/2025/03/27';
 import { EventFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -13,15 +11,11 @@ const EventPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 1, 25, 8) && date < Date.UTC(2025, 2, 1, 8) // 2025-02-25T03:00 (08:00 UTC) to 2025-03-01T03:00 (08:00 UTC)
-        ? <Event20250225 date={date} />
-        : date >= Date.UTC(2025, 2, 5, 5) && date < Date.UTC(2025, 2, 11, 7) // 2025-03-05T00:00 (05:00 UTC) to 2025-03-11T03:00 (07:00 UTC)
-          ? <Event20250305 date={date} />
-          : date >= Date.UTC(2025, 2, 12, 12) && date < Date.UTC(2025, 2, 15, 7) // 2025-03-12T08:00 (12:00 UTC) to 2025-03-15T03:00 (07:00 UTC)
-            ? <Event20250312 date={date} />
-            : date >= Date.UTC(2025, 2, 19, 12) && date < Date.UTC(2025, 2, 27, 7) // 2025-03-19T08:00 (12:00 UTC) to 2025-03-27T03:00 (07:00 UTC)
-              ? <Event20250319 date={date} />
-              : <EventFallback date={date} />
+      {date >= Date.UTC(2025, 2, 19, 12) && date < Date.UTC(2025, 2, 27, 7) // 2025-03-19T08:00 (12:00 UTC) to 2025-03-27T03:00 (07:00 UTC)
+        ? <Event20250319 date={date} />
+        : date >= Date.UTC(2025, 2, 27, 7) && date < Date.UTC(2025, 3, 3, 7) // 2025-03-27T03:00 (07:00 UTC) to 2025-04-03T03:00 (07:00 UTC)
+          ? <Event20250327 date={date} />
+          : <EventFallback date={date} />
       }
     </Suspense>
   );
