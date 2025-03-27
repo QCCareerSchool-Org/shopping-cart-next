5aa9ad3bb2c2d089f218014a783f1a36b0fe0aa5
Dave Welsh <dave@qccareerschool.com>
2025-03-27 08:38:53 -0400

    Added student promos

---
+++ src/app/sites/event/student/_carts/2025/03/27/hero.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+import Desktop from './desktop.jpg';
+import Mobile from './mobile.jpg';
+import { PromoImage } from '@/components/promoImage';
+
+export const Hero20250327: FC = () => {
+  return <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} />;
+};
--- src/app/sites/makeup/student/page.tsx
+++ src/app/sites/makeup/student/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { MakeupStudent20250225 } from './_carts/2025/02/25';
+import { MakeupStudent20250327 } from './_carts/2025/03/27';
 import { MakeupStudentFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -10,8 +10,8 @@ const MakeupStudentPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date < Date.UTC(2025, 2, 27, 7) // 2025-03-27T03:00 (07:00 UTC)
-        ? <MakeupStudent20250225 date={date} />
+      {date < Date.UTC(2025, 3, 3, 7) // 2025-04–03T03:00 (07:00 UTC)
+        ? <MakeupStudent20250327 date={date} />
         : <MakeupStudentFallback date={date} />
       }
     </Suspense>
