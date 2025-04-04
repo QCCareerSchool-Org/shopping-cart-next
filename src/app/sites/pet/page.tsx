59484c96cd4e590e962724e67a8e72121cae1dee
Dave Welsh <dave@qccareerschool.com>
2025-04-04 16:45:28 -0400

    Added April 7 promotions

--- src/app/sites/pet/page.tsx
+++ src/app/sites/pet/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 
-import { Pet20250319 } from './_carts/2025/03/19';
-import { Pet20250327 } from './_carts/2025/03/27';
+import { Pet20250407 } from './_carts/2025/04/07';
 import { PetFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -11,11 +10,9 @@ const PetPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 2, 19, 12) && date < Date.UTC(2025, 2, 27, 7) // 2025-03-19T08:00 (12:00 UTC) to 2025-03-27T03:00 (07:00 UTC)
-        ? <Pet20250319 date={date} />
-        : date >= Date.UTC(2025, 2, 27, 7) && date < Date.UTC(2025, 3, 3, 7) // 2025-03-27T03:00 (07:00 UTC) to 2025-04-03T03:00 (07:00 UTC)
-          ? <Pet20250327 date={date} />
-          : <PetFallback date={date} />
+      {date >= Date.UTC(2025, 3, 7, 12) && date < Date.UTC(2025, 3, 13, 7) // 2025-04-07T08:00 (12:00 UTC) to 2025-04-13T03:00 (07:00 UTC)
+        ? <Pet20250407 date={date} />
+        : <PetFallback date={date} />
       }
     </Suspense>
   );
