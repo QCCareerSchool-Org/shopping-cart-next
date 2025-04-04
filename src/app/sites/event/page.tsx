d909728eb59d3ab0e91ecf4648c9d5ae96a77119
Dave Welsh <dave@qccareerschool.com>
2025-04-04 16:53:26 -0400

    Fixed dates

--- src/app/sites/event/page.tsx
+++ src/app/sites/event/page.tsx
@@ -10,7 +10,7 @@ const EventPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      { date >= Date.UTC(2025, 3, 7, 12) && date < Date.UTC(2025, 3, 13, 7) // 2025-04-07T08:00 (12:00 UTC) to 2025-04-13T03:00 (07:00 UTC)
+      {date >= Date.UTC(2025, 3, 7, 12) && date < Date.UTC(2025, 3, 16, 7) // 2025-04-07T08:00 (12:00 UTC) to 2025-04-16T03:00 (07:00 UTC)
         ? <Event20250407 date={date} />
         : <EventFallback date={date} />
       }
--- src/app/sites/wellness/_carts/2025/04/07/promo.tsx
+++ src/app/sites/wellness/_carts/2025/04/07/promo.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import type { FC } from 'react';
-import { useMemo } from 'react';
 
 import { Hero20250407 } from './hero';
 import { Wellness20250407Modal } from './modal';
@@ -10,8 +9,8 @@ import { Section } from '@/components/section';
 import { useToggle } from '@/hooks/useToggle';
 
 const backgroundColor = '#d7d8dc';
-const firstEndDate = Date.UTC(2025, 3, 1, 7); // 2025-04–01T03:00 (07:00 UTC)
-const secondEndDate = Date.UTC(2025, 3, 3, 7); // 2025-04–03T03:00 (07:00 UTC)
+const lastChanceDate = Date.UTC(2025, 3, 13, 7); // 2025-04–13T03:00 (07:00 UTC)
+const endDate = Date.UTC(2025, 3, 16, 7); // 2025-04–16T03:00 (07:00 UTC)
 
 type Props = {
   date: number;
@@ -19,14 +18,6 @@ type Props = {
 
 export const Wellness20250407Promo: FC<Props> = ({ date }) => {
   const [ showPopup, togglePopup ] = useToggle(false);
-
-  const [ lastChanceDate, endDate ] = useMemo(() => {
-    if (date < firstEndDate) {
-      return [ Date.UTC(2025, 2, 30, 7), firstEndDate ]; // 2025-03-30T03:00 (07:00 UTC)
-    }
-    return [ Date.UTC(2025, 3, 2, 7), secondEndDate ]; // 2025-04-02T03:00 (07:00 UTC)
-  }, [ date ]);
-
   const variant = date >= lastChanceDate ? 'lastChance' : undefined;
 
   const handleClick = (): void => {
