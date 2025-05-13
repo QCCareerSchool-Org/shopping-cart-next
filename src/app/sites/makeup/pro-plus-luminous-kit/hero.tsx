67d53d6c9d072538325619861137a1293ab8aacf
Dave Welsh <dave@qccareerschool.com>
2025-05-13 14:15:46 -0400

    Removed old cart

--- src/app/sites/makeup/pro-plus-luminous-kit/hero.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from 'react';
-
-import Desktop from './desktop.jpg';
-import Mobile from './mobile.jpg';
-import { PromoImage } from '@/components/promoImage';
-
-export const Hero: FC = () => <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} />;
