769fd4fff89a4ecfff1b79038b0d65c64e60ad8c
Dave Welsh <dave@qccareerschool.com>
2025-05-23 12:01:50 -0400

    Changed end date

--- src/app/sites/makeup/page.tsx
+++ src/app/sites/makeup/page.tsx
@@ -11,7 +11,7 @@ const MakeupPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 4, 26, 7) && date < Date.UTC(2025, 4, 31, 7) // 2025-05-26T03:00 (07:00 UTC) to 2025-05-31T03:00 (07:00 UTC)
+      {date >= Date.UTC(2025, 4, 26, 7) && date < Date.UTC(2025, 5, 1, 7) // 2025-05-26T03:00 (07:00 UTC) to 2025-05-01T03:00 (07:00 UTC)
         ? <Makeup20250526 date={date} />
         : date >= Date.UTC(2025, 4, 14, 4) && date < Date.UTC(2025, 4, 25, 7) // 2025-05-14T00:00 (04:00 UTC) to 2025-05-25T03:00 (07:00 UTC)
           ? <Makeup20250514 date={date} />
