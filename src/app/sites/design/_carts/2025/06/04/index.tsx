811f9a027259e939459aa944514f0097146bb946
Dave Welsh <dave@qccareerschool.com>
2025-06-02 16:56:21 -0400

    Added June 4 promotions

---
+++ src/app/sites/design/_carts/2025/06/04/index.tsx
@@ -0,0 +1,32 @@
+import type { FC } from 'react';
+
+import { Design20250604Promo } from './promo';
+import { agreementLinks } from '@/app/sites/design/agreementLinks';
+import { courseGroups } from '@/app/sites/design/courseGroups';
+import { Guarantee } from '@/app/sites/design/guarantee';
+import { BogoDynamicCourseMessage } from '@/components/dynamicCourseMessages/bogo';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const Design20250604: FC<Props> = ({ date }) => {
+  return (
+    <>
+      <Design20250604Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Design School"
+        guarantee={Guarantee}
+        successLink="https://www.qcdesignschool.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        dynamicCourseDescriptions="SHOW"
+        visualPaymentPlans
+        promoCodeDefault="BOGO100"
+        dynamicCourseMessages={[ BogoDynamicCourseMessage ]}
+      />
+    </>
+  );
+};
