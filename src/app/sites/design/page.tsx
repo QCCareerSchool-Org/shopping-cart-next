e232ed2c726033995270bbe0a608c2420058d7fe
Dave Welsh <dave@qccareerschool.com>
2025-06-03 09:14:12 -0400

    Set carts to show

--- src/app/sites/design/page.tsx
+++ src/app/sites/design/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { Design20250522 } from './_carts/2025/05/22';
+import { Design20250604 } from './_carts/2025/06/04';
 import { DesignFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -10,8 +10,8 @@ const DesignPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 4, 22, 7) && date < Date.UTC(2025, 4, 31, 7) // 2025-05-22T03:00 (07:00 UTC) to 2025-05-31T03:00 (07:00 UTC)
-        ? <Design20250522 date={date} />
+      {date >= Date.UTC(2025, 5, 4, 12) && date < Date.UTC(2025, 5, 14, 7) // 2025-06-04T08:00 (12:00 UTC) to 2025-06-14T03:00 (07:00 UTC)
+        ? <Design20250604 date={date} />
         : <DesignFallback date={date} />
       }
     </Suspense>
