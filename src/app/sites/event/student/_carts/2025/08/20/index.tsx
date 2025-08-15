309bee3af903d1b6ee017e20de3738f2f081bf80
Dave Welsh <dave@qccareerschool.com>
2025-08-15 10:58:37 -0400

    Added student carts

---
+++ src/app/sites/event/student/_carts/2025/08/20/index.tsx
@@ -0,0 +1,31 @@
+import type { FC } from 'react';
+
+import { EventStudent20250820Promo } from './promo';
+import { agreementLinks } from '../../../../../agreementLinks';
+import { courseGroups } from '../../../../../courseGroups';
+import { Guarantee } from '../../../../../guarantee';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const EventStudent20250820: FC<Props> = ({ date }) => {
+  return (
+    <>
+      <EventStudent20250820Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Event School"
+        guarantee={Guarantee}
+        successLink="https://www.qceventplanning.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        student
+        dynamicCourseDescriptions="HIDE"
+        visualPaymentPlans
+        promoCodeDefault="SAVE60"
+      />
+    </>
+  );
+};
