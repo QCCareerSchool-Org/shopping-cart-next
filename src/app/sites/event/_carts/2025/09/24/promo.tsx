1035f7762ac599a59fd4fc41c9a97f60d4f7c0b8
Dave Welsh <dave@qccareerschool.com>
2025-09-24 09:02:53 -0400

    Updated backup color

--- src/app/sites/event/_carts/2025/09/24/promo.tsx
+++ src/app/sites/event/_carts/2025/09/24/promo.tsx
@@ -8,7 +8,7 @@ import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTime
 import { Section } from '@/components/section';
 import { useToggle } from '@/hooks/useToggle';
 
-const backgroundColor = '#fdc31e';
+const backgroundColor = '#b2b3ae';
 const firstLastChanceDate = Date.UTC(2025, 8, 26, 7); // 2025-09-26T03:00 (07:00 UTC)
 const firstEndDate = Date.UTC(2025, 8, 27, 7); // 2025-09-27T03:00 (07:00 UTC)
 const secondLastChanceDate = Date.UTC(2025, 8, 30, 7); // 2025-09-30T03:00 (07:00 UTC)
