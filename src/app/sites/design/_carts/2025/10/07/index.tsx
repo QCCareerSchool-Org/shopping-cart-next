e4e5e9fca0102b3fb6e30e9fc608c94c0533b502
Dave Welsh <dave@qccareerschool.com>
2025-10-02 09:28:35 -0400

    Updated dynamic message

--- src/app/sites/design/_carts/2025/10/07/index.tsx
+++ src/app/sites/design/_carts/2025/10/07/index.tsx
@@ -4,7 +4,7 @@ import { Design20251007Promo } from './promo';
 import { agreementLinks } from '@/app/sites/design/agreementLinks';
 import { courseGroups } from '@/app/sites/design/courseGroups';
 import { Guarantee } from '@/app/sites/design/guarantee';
-import { Save50CourseMessage } from '@/components/dynamicCourseMessages/save50';
+import { FreeColorConsultantMessage } from '@/components/dynamicCourseMessages/freeColorConsultant';
 import { Form } from '@/components/form';
 
 type Props = {
@@ -24,7 +24,7 @@ export const Design20251007: FC<Props> = ({ date }) => {
         agreementLinks={agreementLinks}
         dynamicCourseDescriptions="SHOW"
         visualPaymentPlans
-        dynamicCourseMessages={[ Save50CourseMessage ]}
+        dynamicCourseMessages={[ FreeColorConsultantMessage ]}
         promoCodeDefault="FREECOLOR"
         hideCourseTable
       />
