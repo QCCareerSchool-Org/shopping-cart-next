d027c771f97525ed36166b2c8b101128d789e5a9
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-16 12:13:40 -0400

    Update Design fallback modal

--- src/app/sites/design/_carts/fallback/modal.tsx
+++ src/app/sites/design/_carts/fallback/modal.tsx
@@ -8,9 +8,16 @@ type Props = {
 
 export const DesignFallbackModal: FC<Props> = props => (
   <Modal show={props.show} onHide={props.onHide}>
-    <Modal.Header closeButton>Limited-Time Offer</Modal.Header>
+    <Modal.Header closeButton>Start Your Design Career in Months!</Modal.Header>
     <Modal.Body>
-      <p className="lead mb-0">For a limited time, get the <strong>Color Consultant Course FREE</strong> when you also enroll in any other course of equal or greater value.</p>
+      <p>Enroll in any design course today, and receive our Color Consultant course FREE!</p>
+      <p>Expand your expertise and advance your career with professional training in color theory and application. You&apos;ll learn how to:</p>
+      <ul>
+        <li className="mb-2">Confidently select palettes that suit any space</li>
+        <li className="mb-2">Understand undertones, lighting, and how they affect color choices</li>
+        <li>Design cohesive interiors for a variety of clients and budgets</li>
+      </ul>
+      <p>Plus, enjoy <strong>50% off additional courses</strong> to keep growing your skills and career.</p>
     </Modal.Body>
   </Modal>
 );
