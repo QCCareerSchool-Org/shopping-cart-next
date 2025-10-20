f9d3ef764a5e81b2ad96fcfe77e034d62256443d
Dave Welsh <dave@qccareerschool.com>
2025-10-20 11:59:23 -0400

    Added promo code

--- src/app/sites/design/professional-organizing-overview/page.tsx
+++ src/app/sites/design/professional-organizing-overview/page.tsx
@@ -22,6 +22,7 @@ const ProfessionalOrganizingOverview: PageComponent = async ({ searchParams }) =
         agreementLinks={agreementLinks}
         dynamicCourseDescriptions="SHOW"
         visualPaymentPlans
+        promoCodeDefault="100OFF"
         dynamicCourseMessages={[ Save50CourseMessage ]}
         hideCourseTable
       />
