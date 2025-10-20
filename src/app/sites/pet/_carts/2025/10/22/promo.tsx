6976a7ad96bc6f7e2904554edaa480f5829e1cd1
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-20 14:50:18 -0400

    Update Pet background colour

--- src/app/sites/pet/_carts/2025/10/22/promo.tsx
+++ src/app/sites/pet/_carts/2025/10/22/promo.tsx
@@ -8,7 +8,7 @@ import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTime
 import { Section } from '@/components/section';
 import { useToggle } from '@/hooks/useToggle';
 
-const backgroundColor = '#030406';
+const backgroundColor = '#8959FD';
 const lastChanceDate = Date.UTC(2025, 9, 31, 7); // 2025-10-31T03:00 (07:00 UTC)
 const endDate = Date.UTC(2025, 10, 1, 7); // 2025-11-01T03:00 (07:00 UTC)
 
