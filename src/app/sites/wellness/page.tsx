66c7b76611c7c4e6b829eab4f511127df29f8c2d
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-20 15:18:44 -0400

    Create Wellness cart

--- src/app/sites/wellness/page.tsx
+++ src/app/sites/wellness/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { Wellness20251007 } from './_carts/2025/10/07';
+import { Wellness20251022 } from './_carts/2025/10/22';
 import { WellnessFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -10,8 +10,8 @@ const DesignPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 9, 7, 12) && date < Date.UTC(2025, 9, 18, 7) // 2025-10-07T08:00 (12:00 UTC) to 2025-10-18T03:00 (07:00 UTC)
-        ? <Wellness20251007 date={date} />
+      {date >= Date.UTC(2025, 9, 22, 16) && date < Date.UTC(2025, 9, 31, 7) // 2025-10-22T12:00 (16:00 UTC) to 2025-10-31T03:00 (07:00 UTC)
+        ? <Wellness20251022 date={date} />
         : <WellnessFallback date={date} />
       }
     </Suspense>
