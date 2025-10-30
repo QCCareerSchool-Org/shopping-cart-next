268a413052641956b491e4795df6fca7066c41ab
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-30 16:52:07 -0400

    Create Makeup student cart

---
+++ src/app/sites/makeup/student/_carts/2025/11/03/promo.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import type { FC } from 'react';
+
+import { Hero20251103 } from './hero';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+
+const backgroundColor = '#0d0d0d';
+const lastChanceDate = Date.UTC(2025, 10, 7, 7); // 2025-11-07T03:00 (07:00 UTC)
+const endDate = Date.UTC(2025, 10, 8, 7); // 2025-11-08T03:00 (07:00 UTC)
+
+type Props = {
+  date: number;
+};
+
+export const MakeupStudent20251103Promo: FC<Props> = ({ date }) => (
+  <>
+    <Section style={{ backgroundColor }} noPadding>
+      <Hero20251103 />
+    </Section>
+    <CountDownTimerWrapper
+      date={date}
+      showDate={lastChanceDate}
+      endDate={endDate}
+      message={<span style={{ textTransform: 'uppercase' }}>This special offer ends soon!</span>}
+      className="bg-black text-light"
+    />
+  </>
+);
