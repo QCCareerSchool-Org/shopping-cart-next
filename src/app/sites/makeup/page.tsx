6b492716fddbad597260dd4780005892c2d8865f
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-10-30 14:26:45 -0400

    Create Makeup cart

--- src/app/sites/makeup/page.tsx
+++ src/app/sites/makeup/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 
-import { Makeup20251022 } from './_carts/2025/10/22';
+import { Makeup20251103 } from './_carts/2025/11/03';
 import { MakeupFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -10,8 +10,8 @@ const MakeupPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 9, 22, 4) && date < Date.UTC(2025, 10, 1, 7) // 2025-10-22T00:00 (04:00 UTC) to 2025-11-01T03:00 (07:00 UTC)
-        ? <Makeup20251022 date={date} />
+      {date >= Date.UTC(2025, 9, 30, 12) && date < Date.UTC(2025, 10, 8, 7) // 2025-11-03T08:00 (12:00 UTC) to 2025-11-08T03:00 (07:00 UTC)
+        ? <Makeup20251103 date={date} />
         : <MakeupFallback date={date} />
       }
     </Suspense>
