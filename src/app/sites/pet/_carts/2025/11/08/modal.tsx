f26f2b3374c84e9d1f0f6e052511a73caf1b5515
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-11-06 12:56:30 -0500

    Create Pet cart

---
+++ src/app/sites/pet/_carts/2025/11/08/modal.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import type { FC } from 'react';
+import { Modal } from 'react-bootstrap';
+
+type Props = {
+  show: boolean;
+  onHide: () => void;
+};
+
+export const Pet20251108Modal: FC<Props> = props => {
+  return (
+    <Modal show={props.show} onHide={props.onHide}>
+      <Modal.Header closeButton>Start Your Pet Care Career</Modal.Header>
+      <Modal.Body>
+        <p>For a limited time, enroll in any pet course and get a second course FREE!</p>
+        <hr />
+        <h6 className="sans-serif">Learn From Expert Instructors</h6>
+        <p>We&apos;ve hand-selected the top pet care professionals in the industry to guide you through your course material.</p>
+        <h6 className="sans-serif">Industry-Recognized Certifications</h6>
+        <p>Graduate with professional pet industry certifications and open up a world of career & business opportunities.</p>
+        <h6 className="sans-serif">Business Training Included</h6>
+        <p>Each course includes built-in business training to help you launch your career and grow your clientele as a newly certified trainer, groomer or daycare owner.</p>
+        <h6 className="sans-serif">Lifetime Access to Course Content & Updates</h6>
+        <p>Enjoy lifetime access to your course materials and updates, keeping you ahead of industry trends long after graduation.</p>
+        <h6 className="sans-serif">Free First Aid Training</h6>
+        <p>As part of QC Pet Studies, you&apos;ll receive FREE Pet First Aid training, which provides essential knowledge to help you respond to common pet emergencies and ensure the safety of your clients&apos; pets.</p>
+      </Modal.Body>
+    </Modal>
+  );
+};
---
+++ src/app/sites/pet/_carts/2025/11/08/promo.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import type { FC } from 'react';
+
+import { Banner } from './banner';
+import { Hero20251108 } from './hero';
+import { Pet20251108Modal } from './modal';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+import { useToggle } from '@/hooks/useToggle';
+
+const backgroundColor = '#332E2E';
+const lastChanceDate = Date.UTC(2025, 10, 14, 8); // 2025-11-14T03:00 (08:00 UTC)
+const endDate = Date.UTC(2025, 10, 15, 8); // 2025-11-15T03:00 (08:00 UTC)
+
+type Props = {
+  date: number;
+};
+
+export const Pet20251108Promo: FC<Props> = ({ date }) => {
+  const [ showPopup, togglePopup ] = useToggle(false);
+  const variant = date >= lastChanceDate ? 'lastChance' : undefined;
+
+  const handleClick = (): void => {
+    togglePopup();
+  };
+
+  return (
+    <>
+      <Section style={{ backgroundColor }} noPadding>
+        <a href="#courses">
+          <Hero20251108 variant={variant} />
+        </a>
+      </Section>
+      <Pet20251108Modal show={showPopup} onHide={handleClick} />
+      <CountDownTimerWrapper
+        date={date}
+        showDate={lastChanceDate}
+        endDate={endDate}
+        message={<span style={{ textTransform: 'uppercase' }}>This exclusive offer ends soon!</span>}
+        className="bg-primary text-light"
+      />
+      <Banner variant={variant} onClick={handleClick} />
+    </>
+  );
+};
