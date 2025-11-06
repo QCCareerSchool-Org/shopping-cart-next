cc4b94cb0cc82de233ec7c796366b3cda5d39101
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-11-06 10:44:45 -0500

    Create Design student cart

---
+++ src/app/sites/design/student/_carts/2025/11/08/promo.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import type { FC } from 'react';
+
+import { Hero20251108 } from './hero';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+
+const backgroundColor = '#0f141a';
+const lastChanceDate = Date.UTC(2025, 10, 14, 8); // 2025-11-14T03:00 (08:00 UTC)
+const endDate = Date.UTC(2025, 10, 15, 8); // 2025-11-15T03:00 (08:00 UTC)
+
+type Props = {
+  date: number;
+};
+
+export const DesignStudent20251108Promo: FC<Props> = ({ date }) => (
+  <>
+    <Section style={{ backgroundColor }} noPadding>
+      <Hero20251108 />
+    </Section>
+    <CountDownTimerWrapper
+      date={date}
+      showDate={lastChanceDate}
+      endDate={endDate}
+      message={<span style={{ textTransform: 'uppercase' }}>This special offer ends soon!</span>}
+      className="bg-black text-light"
+    />
+  </>
+);
