4db229478c5963b0bc8e2fccb824ce26d3715d9b
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-11-06 12:42:30 -0500

    Create Makeup cart

--- src/app/sites/makeup/page.tsx
+++ src/app/sites/makeup/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 
-import { Makeup20251022 } from './_carts/2025/10/22';
 import { Makeup20251103 } from './_carts/2025/11/03';
+import { Makeup20251108 } from './_carts/2025/11/08';
 import { MakeupFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -11,10 +11,10 @@ const MakeupPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 9, 22, 4) && date < Date.UTC(2025, 10, 1, 7) // 2025-10-22T00:00 (04:00 UTC) to 2025-11-01T03:00 (07:00 UTC)
-        ? <Makeup20251022 date={date} />
-        : date >= Date.UTC(2025, 10, 3, 13) && date < Date.UTC(2025, 10, 8, 8) // 2025-11-03T08:00 (13:00 UTC) to 2025-11-08T03:00 (08:00 UTC)
-          ? <Makeup20251103 date={date} />
+      {date >= Date.UTC(2025, 10, 3, 13) && date < Date.UTC(2025, 10, 8, 8) // 2025-11-03T08:00 (13:00 UTC) to 2025-11-08T03:00 (08:00 UTC)
+        ? <Makeup20251103 date={date} />
+        : date >= Date.UTC(2025, 10, 8, 8) && date < Date.UTC(2025, 10, 15, 8) // 2025-11-08T03:00 (08:00 UTC) to 2025-11-15T03:00 (08:00 UTC)
+          ? <Makeup20251108 date={date} />
           : <MakeupFallback date={date} />
       }
     </Suspense>
