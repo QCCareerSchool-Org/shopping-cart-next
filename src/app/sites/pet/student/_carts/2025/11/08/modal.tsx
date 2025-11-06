a130490c5ffc00e63d8500d1f225ff60f046d8b8
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-11-06 10:56:26 -0500

    Create Pet student cart

---
+++ src/app/sites/pet/student/_carts/2025/11/08/modal.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import type { FC } from 'react';
+import { Modal } from 'react-bootstrap';
+
+type Props = {
+  show: boolean;
+  onHide: () => void;
+};
+
+export const PetStudent20251108Modal: FC<Props> = props => (
+  <Modal show={props.show} onHide={props.onHide}>
+    <Modal.Header closeButton>Start Your Pet Care Career</Modal.Header>
+    <Modal.Body>
+      <p><strong>Get 60% off Dog Training or Dog Behavior!</strong></p>
+      <p>Expand your pet career today and save 60% on Dog Training or Dog Behavior. Plus, enjoy 50% off all other courses.</p>
+    </Modal.Body>
+  </Modal>
+);
