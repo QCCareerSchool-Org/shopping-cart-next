cbedb91324b283f36627b853d69a23c6a593ab6d
Dave Welsh <dave@qccareerschool.com>
2025-11-18 14:40:57 -0500

    Reused previous student carts

--- src/app/sites/event/student/page.tsx
+++ src/app/sites/event/student/page.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
 
-import { EventStudent20251103 } from './_carts/2025/11/03';
 import { EventStudent20251108 } from './_carts/2025/11/08';
 import { EventStudentFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
@@ -11,11 +10,9 @@ const EventStudentPage: PageComponent = async ({ searchParams }) => {
 
   return (
     <Suspense>
-      {date >= Date.UTC(2025, 10, 3, 13) && date < Date.UTC(2025, 10, 8, 8) // 2025-11-03T08:00 (13:00 UTC) to 2025-11-08T03:00 (08:00 UTC)
-        ? <EventStudent20251103 date={date} />
-        : date >= Date.UTC(2025, 10, 8, 8) && date < Date.UTC(2025, 10, 17, 4, 59) // 2025-11-08T03:00 (8:00 UTC) to 2025-11-16T023:59 (4:59 UTC)
-          ? <EventStudent20251108 date={date} />
-          : <EventStudentFallback date={date} />
+      {date >= Date.UTC(2025, 10, 17, 5) && date < Date.UTC(2025, 10, 29, 8) // 2025-11-17T00:00 (05:00 UTC) to 2025-11-29T03:00 (08:00 UTC)
+        ? <EventStudent20251108 date={date} />
+        : <EventStudentFallback date={date} />
       }
     </Suspense>
   );
