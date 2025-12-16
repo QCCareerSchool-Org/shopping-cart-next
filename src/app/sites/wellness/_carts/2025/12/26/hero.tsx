83daf1407889e8b21117072fd722e1354d4668aa
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-16 13:11:16 -0500

    Create Wellness cart

---
+++ src/app/sites/wellness/_carts/2025/12/26/hero.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import type { FC } from 'react';
+
+import DesktopEnds from './desktop-ends.jpg';
+import DesktopUKEnds from './desktop-uk-ends.jpg';
+import DesktopUK from './desktop-uk.jpg';
+import Desktop from './desktop.jpg';
+import MobileEnds from './mobile-ends.jpg';
+import MobileUKEnds from './mobile-uk-ends.jpg';
+import MobileUK from './mobile-uk.jpg';
+import Mobile from './mobile.jpg';
+import { PromoImage } from '@/components/promoImage';
+import { usePriceState } from '@/hooks/usePriceState';
+
+type Props = {
+  variant?: 'lastChance';
+};
+
+export const Hero20251226: FC<Props> = ({ variant }) => {
+  const priceState = usePriceState();
+
+  const [ desktopSrc, mobileSrc ] = variant === 'lastChance'
+    ? priceState?.currency.code === 'GBP' ? [ DesktopUKEnds, MobileUKEnds ] : [ DesktopEnds, MobileEnds ]
+    : priceState?.currency.code === 'GBP' ? [ DesktopUK, MobileUK ] : [ Desktop, Mobile ];
+
+  return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
+};
