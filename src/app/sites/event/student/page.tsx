9b01dfa838d26786cfb0e1f8bb701c6c957f460a
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-16 17:14:57 -0500

    Create Event student cart

--- src/app/sites/event/student/page.tsx
+++ src/app/sites/event/student/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 
 import { EventStudent20251226 } from './_carts/2025/12/26';
+import { EventStudent20260107 } from './_carts/2026/01/07';
 import { EventStudentFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -12,7 +13,9 @@ const EventStudentPage: PageComponent = async ({ searchParams }) => {
     <Suspense>
       { date >= Date.UTC(2025, 11, 26, 8) && date < Date.UTC(2026, 0, 3, 8) // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
         ? <EventStudent20251226 date={date} />
-        : <EventStudentFallback date={date} />
+        : date >= Date.UTC(2026, 0, 7, 8) && date < Date.UTC(2026, 0, 17, 8) // 2026-01-07T03:00 (8:00 UTC) to 2026-01-17T03:00 (8:00 UTC)
+          ? <EventStudent20260107 date={date} />
+          : <EventStudentFallback date={date} />
       }
     </Suspense>
   );
