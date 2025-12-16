b07f47f31761815cbbc2f2869405f498820c9973
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-16 16:11:56 -0500

    Create Event cart

---
+++ src/app/sites/event/_carts/2026/01/07/index.tsx
@@ -0,0 +1,30 @@
+import type { FC } from 'react';
+
+import { Event20260107Promo } from './promo';
+import { agreementLinks } from '@/app/sites/event/agreementLinks';
+import { courseGroups } from '@/app/sites/event/courseGroups';
+import { Guarantee } from '@/app/sites/event/guarantee';
+import { BogoDynamicCourseMessage } from '@/components/dynamicCourseMessages/bogo';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const Event20260107: FC<Props> = ({ date }) => (
+  <>
+    <Event20260107Promo date={date} />
+    <Form
+      date={date}
+      courseGroups={courseGroups}
+      school="QC Event School"
+      guarantee={Guarantee}
+      successLink="https://www.qceventplanning.com/welcome-to-the-school"
+      agreementLinks={agreementLinks}
+      dynamicCourseDescriptions="HIDE"
+      visualPaymentPlans
+      promoCodeDefault="BOGO"
+      dynamicCourseMessages={[ BogoDynamicCourseMessage ]}
+    />
+  </>
+);
