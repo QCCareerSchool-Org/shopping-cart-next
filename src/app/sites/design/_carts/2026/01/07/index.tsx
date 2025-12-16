3e66ae5ac495121ee1305d81cd41fc29d8252835
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-16 16:03:06 -0500

    Create Design cart

---
+++ src/app/sites/design/_carts/2026/01/07/index.tsx
@@ -0,0 +1,33 @@
+import type { FC } from 'react';
+
+import { Design20260107Promo } from './promo';
+import { agreementLinks } from '@/app/sites/design/agreementLinks';
+import { courseGroups } from '@/app/sites/design/courseGroups';
+import { Guarantee } from '@/app/sites/design/guarantee';
+import { BogoDynamicCourseMessage } from '@/components/dynamicCourseMessages/bogo';
+import { Form } from '@/components/form';
+
+type Props = {
+  date: number;
+};
+
+export const Design20260107: FC<Props> = ({ date }) => {
+  return (
+    <>
+      <Design20260107Promo date={date} />
+      <Form
+        date={date}
+        courseGroups={courseGroups}
+        school="QC Design School"
+        guarantee={Guarantee}
+        successLink="https://www.qcdesignschool.com/welcome-to-the-school"
+        agreementLinks={agreementLinks}
+        dynamicCourseDescriptions="SHOW"
+        visualPaymentPlans
+        dynamicCourseMessages={[ BogoDynamicCourseMessage ]}
+        promoCodeDefault="BOGO"
+        hideCourseTable
+      />
+    </>
+  );
+};
