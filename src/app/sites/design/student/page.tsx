c9878b3eb9ca07cfd74cff4dcecd2270a5752ade
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-16 17:10:06 -0500

    Create Design student cart

--- src/app/sites/design/student/page.tsx
+++ src/app/sites/design/student/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 
 import { DesignStudent20251226 } from './_carts/2025/12/26';
+import { DesignStudent20260107 } from './_carts/2026/01/07';
 import { DesignStudentFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -12,7 +13,9 @@ const DesignStudentPage: PageComponent = async ({ searchParams }) => {
     <Suspense>
       { date >= Date.UTC(2025, 11, 26, 8) && date < Date.UTC(2026, 0, 3, 8) // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
         ? <DesignStudent20251226 date={date} />
-        : <DesignStudentFallback date={date} />
+        : date >= Date.UTC(2026, 0, 7, 8) && date < Date.UTC(2026, 0, 17, 8) // 2026-01-07T03:00 (8:00 UTC) to 2026-01-17T03:00 (8:00 UTC)
+          ? <DesignStudent20260107 date={date} />
+          : <DesignStudentFallback date={date} />
       }
     </Suspense>
   );
