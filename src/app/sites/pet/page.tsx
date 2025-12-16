4aa1bd80b057afce8e49417be05dfcc3e988a285
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-16 16:38:46 -0500

    Create Pet cart

--- src/app/sites/pet/page.tsx
+++ src/app/sites/pet/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 
 import { Pet20251211 } from './_carts/2025/12/11';
 import { Pet20251226 } from './_carts/2025/12/26';
+import { Pet20260107 } from './_carts/2026/01/07';
 import { PetFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
@@ -15,7 +16,9 @@ const PetPage: PageComponent = async ({ searchParams }) => {
         ? <Pet20251211 date={date} />
         : date >= Date.UTC(2025, 11, 26, 8) && date < Date.UTC(2026, 0, 3, 8) // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
           ? <Pet20251226 date={date} />
-          : <PetFallback date={date} />
+          : date >= Date.UTC(2026, 0, 7, 8) && date < Date.UTC(2026, 0, 17, 8) // 2026-01-07T03:00 (8:00 UTC) to 2026-01-17T03:00 (8:00 UTC)
+            ? <Pet20260107 date={date} />
+            : <PetFallback date={date} />
       }
     </Suspense>
   );
