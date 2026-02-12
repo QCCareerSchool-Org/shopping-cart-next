fb416bae11177d48e3916ce93a8446f8603ba31d
Dave Welsh <dave@qccareerschool.com>
2026-02-12 10:59:20 -0500

    Updated images

--- src/app/sites/pet/_carts/2026/02/13/hero.tsx
+++ src/app/sites/pet/_carts/2026/02/13/hero.tsx
@@ -1,6 +1,8 @@
 import type { FC } from 'react';
 
+import DesktopEnds from './desktop-ends.jpg';
 import Desktop from './desktop.jpg';
+import MobileEnds from './mobile-ends.jpg';
 import Mobile from './mobile.jpg';
 import { PromoImage } from '@/components/promoImage';
 
@@ -8,6 +10,10 @@ interface Props {
   variant?: 'lastChance';
 }
 
-export const Hero20260213: FC<Props> = () => {
-  return <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} />;
+export const Hero20260213: FC<Props> = ({ variant }) => {
+  const [ desktopSrc, mobileSrc ] = variant === 'lastChance'
+    ? [ DesktopEnds, MobileEnds ]
+    : [ Desktop, Mobile ];
+
+  return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
 };
