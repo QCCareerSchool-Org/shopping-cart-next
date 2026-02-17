f0555a86b4def146afd995d4f6feec758c6c5dc8
Dave Welsh <dave@qccareerschool.com>
2026-02-17 11:21:01 -0500

    Added banenrs and upscaled image

--- src/app/sites/event/_carts/fallback/hero.tsx
+++ src/app/sites/event/_carts/fallback/hero.tsx
@@ -1,9 +1,9 @@
 import type { FC } from 'react';
 
-import Desktop from './desktop.jpg';
-import Mobile from './mobile.jpg';
+import Desktop from './desktop.webp';
+import Mobile from './mobile.webp';
 import { PromoImage } from '@/components/promoImage';
 
 export const Hero: FC = () => {
-  return <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} />;
+  return <PromoImage desktopSrc={Desktop} mobileSrc={Mobile} maxWidth={1055} />;
 };
