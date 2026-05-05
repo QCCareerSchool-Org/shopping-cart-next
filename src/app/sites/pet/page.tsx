6f8b226829729339f82f98694308a1877b3ed22c
Dave Welsh <dave@qccareerschool.com>
2026-05-05 13:36:04 -0400

    Added pet promotion

--- src/app/sites/pet/page.tsx
+++ src/app/sites/pet/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from 'react';
 
-import { Pet20260422 } from './_carts/2026/04/22';
+import { Pet20260506 } from './_carts/2026/05/06';
 import { PetFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
-import { april22 } from '@/periods';
+import { may06 } from '@/periods';
 import type { PageComponent } from '@/serverComponent';
 
 const PetPage: PageComponent = async props => {
@@ -12,8 +12,8 @@ const PetPage: PageComponent = async props => {
 
   return (
     <Suspense>
-      {april22.contains(date)
-        ? <Pet20260422 date={date} period={april22.toDTO()} />
+      {may06.contains(date)
+        ? <Pet20260506 date={date} period={may06.toDTO()} />
         : <PetFallback date={date} />
       }
     </Suspense>
