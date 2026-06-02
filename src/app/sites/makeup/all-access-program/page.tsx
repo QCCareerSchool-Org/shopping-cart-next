17f4d049c5206c9fe4963d63a8187fa1597a7fbd
Angela Li <angelayxli@gmail.com>
2026-06-02 11:03:35 -0400

    added makeup course code to form

--- src/app/sites/makeup/all-access-program/page.tsx
+++ src/app/sites/makeup/all-access-program/page.tsx
@@ -2,12 +2,18 @@ import { agreementLinks } from '../agreementLinks';
 import { Guarantee } from '../guarantee';
 import { MakeupFreeMasterclassPromo } from './promo';
 import { Form } from '@/components/form';
+import type { CourseGroup } from '@/domain/courseGroup';
 import { getDate } from '@/lib/getDate';
 import type { PageComponent } from '@/serverComponent';
 
 const MakeupAllAccessPage: PageComponent = async props => {
   const searchParams = await props.searchParams;
   const date = await getDate(searchParams.date);
+  const courseGroups: CourseGroup[] = [
+    {
+      items: [ { name: 'All-Access Program', code: 'AM' } ],
+    },
+  ];
   return (
     <>
       <MakeupFreeMasterclassPromo date={date} />
