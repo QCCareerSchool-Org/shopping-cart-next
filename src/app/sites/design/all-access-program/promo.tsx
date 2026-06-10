11d96b0dba1ad9a0a89ec2dda11567bf9df7cd10
Daphne Huang <daphnehlj25@gmail.com>
2026-06-10 12:36:50 -0700

    bug fixes and added new design files

--- src/app/sites/design/all-access-program/promo.tsx
+++ src/app/sites/design/all-access-program/promo.tsx
@@ -2,11 +2,8 @@
 
 import type { FC } from 'react';
 
-// import { AllAccessHero } from './hero';
-import HeroMobile from './hero-mobile.jpg';
-import HeroDesktop from './hero.jpg';
+import { AllAccessHero } from './hero';
 import { AllAccessModal } from './modal';
-import { BackgroundImage } from '@/components/backgroundImage';
 import { Section } from '@/components/section';
 import { useAddressState } from '@/hooks/useAddressState';
 import { useToggle } from '@/hooks/useToggle';
@@ -26,19 +23,7 @@ export const AllAccessPromo: FC = () => {
     <>
       <Section style={{ backgroundColor }} noPadding>
         <div onClick={handleClick} style={{ cursor: 'pointer' }}>
-          { /* <AllAccessHero /> */ }
-          <section className="bg-navy text-white text-center">
-            <BackgroundImage src={HeroDesktop} mobile={{ src: HeroMobile, breakpoint: 'lg' }} priority />
-            <div className="container">
-              <div className="eyebrow text-shadow mb-3"><strong>Best Value:</strong> Earn Your Elite Design Professional Certification</div>
-              <h1 className="text-shadow mb-4">Join the All-Access Program</h1>
-              <p className="lead fw-medium text-shadow mb-5">Save 68% or More on Tuition &amp; Maximize Your Earning Potential</p>
-              <div className="d-flex justify-content-center gap-4">
-                <a className="btn btn-primary shadow">Become an Elite Design Professional</a>
-                <a href="#included" className="btn btn-outline-light shadow">See 9 Included Courses</a>
-              </div>
-            </div>
-          </section>
+          <AllAccessHero />
         </div>
       </Section>
       <Section className="text-center">
