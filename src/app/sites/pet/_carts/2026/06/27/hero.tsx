f5c27234263c1bef4980c4a44c2db5b203688f46
Angela Li <angelayxli@gmail.com>
2026-06-29 16:09:37 -0400

    add desktop UK images to pet cart

--- src/app/sites/pet/_carts/2026/06/27/hero.tsx
+++ src/app/sites/pet/_carts/2026/06/27/hero.tsx
@@ -7,6 +7,8 @@ import MobileEnds from './desktop-mobile-ends.jpg';
 import MobileUKEnds from './desktop-mobile-uk-ends.jpg';
 import MobileUK from './desktop-mobile-uk.jpg';
 import Mobile from './desktop-mobile.jpg';
+import DesktopUKEnds from './desktop-uk-ends.jpg';
+import DesktopUK from './desktop-uk.jpg';
 import DesktopUSEnds from './desktop-us-ends.jpg';
 import DesktopUS from './desktop-us.jpg';
 import Desktop from './desktop.jpg';
@@ -24,11 +26,11 @@ interface Props {
 export const Hero20260627: FC<Props> = ({ variant }) => {
   const priceState = usePriceState();
   const [ desktopSrc, mobileSrc ] = variant === 'lastChance'
-    ? priceState?.currency.code === 'GBP' ? [ DesktopEnds, MobileUKEnds ] :
+    ? priceState?.currency.code === 'GBP' ? [ DesktopUKEnds, MobileUKEnds ] :
       priceState?.currency.code === 'USD' ? [ DesktopUSEnds, MobileUSEnds ] :
         priceState?.currency.code === 'CAD' ? [ DesktopCAEnds, MobileCAEnds ] :
           [ DesktopEnds, MobileEnds ]
-    : priceState?.currency.code === 'GBP' ? [ Desktop, MobileUK ] :
+    : priceState?.currency.code === 'GBP' ? [ DesktopUK, MobileUK ] :
       priceState?.currency.code === 'USD' ? [ DesktopUS, MobileUS ] :
         priceState?.currency.code === 'CAD' ? [ DesktopCA, MobileCA ] :
           [ Desktop, Mobile ];
