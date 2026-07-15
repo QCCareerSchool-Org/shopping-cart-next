29b38a0f3aa3f91b283143ff4f946b16af14681c
Daphne Huang <daphnehlj25@gmail.com>
2026-07-15 12:39:00 -0700

    update makeup modal

--- src/app/sites/makeup/page.tsx
+++ src/app/sites/makeup/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 
-import { Makeup20260409 } from './_carts/2026/04/09';
 import { Makeup20260708 } from './_carts/2026/07/08';
+import { Makeup20260722 } from './_carts/2026/07/22';
 import { MakeupFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
 import { july22, july8 } from '@/periods';
@@ -14,7 +14,7 @@ const MakeupPage: PageComponent = async props => {
   return (
     <Suspense>
       {july22.contains(date)
-        ? <Makeup20260409 date={date} period={july22.toDTO()} />
+        ? <Makeup20260722 date={date} period={july22.toDTO()} />
         : july8.contains(date)
           ? <Makeup20260708 date={date} period={july8.toDTO()} />
           : <MakeupFallback date={date} />
