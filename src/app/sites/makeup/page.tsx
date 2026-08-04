3b045805ce5d5b961c67c841111ee31b7cff59c0
Angela Li <angelayxli@gmail.com>
2026-08-04 09:34:27 -0400

    swap out makeup promo

--- src/app/sites/makeup/page.tsx
+++ src/app/sites/makeup/page.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react';
 
-import { Makeup20260708 } from './_carts/2026/07/08';
+import { Makeup20260506 } from './_carts/2026/05/06';
 import { Makeup20260722 } from './_carts/2026/07/22';
 import { MakeupFallback } from './_carts/fallback';
 import { getDate } from '@/lib/getDate';
-import { july22, july8 } from '@/periods';
+import { august06, july22, july8 } from '@/periods';
 import type { PageComponent } from '@/serverComponent';
 
 const MakeupPage: PageComponent = async props => {
@@ -13,10 +13,10 @@ const MakeupPage: PageComponent = async props => {
 
   return (
     <Suspense>
-      {july22.contains(date)
-        ? <Makeup20260722 date={date} period={july22.toDTO()} />
-        : july8.contains(date)
-          ? <Makeup20260708 date={date} period={july8.toDTO()} />
+      {august06.contains(date)
+        ? <Makeup20260506 date={date} period={july22.toDTO()} />
+        : july22.contains(date)
+          ? <Makeup20260722 date={date} period={july8.toDTO()} />
           : <MakeupFallback date={date} />
       }
     </Suspense>
