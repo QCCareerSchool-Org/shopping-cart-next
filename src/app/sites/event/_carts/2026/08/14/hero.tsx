f1ce113b1c6fae88c375010149fd9a62a2dd2712
Angela Li <angelayxli@gmail.com>
2026-08-07 09:44:01 -0400

    schedule aug 14 promos for general carts

---
+++ src/app/sites/event/_carts/2026/08/14/hero.tsx
@@ -0,0 +1,16 @@
+import type { FC } from 'react';
+
+import Desktop from './desktop.jpg';
+import Mobile from './mobile.jpg';
+import { PromoImage } from '@/components/promoImage';
+
+interface Props {
+  variant?: 'lastChance';
+}
+
+export const Hero20260814: FC<Props> = () => {
+
+  const [ desktopSrc, mobileSrc ] = [ Desktop, Mobile ];
+
+  return <PromoImage desktopSrc={desktopSrc} mobileSrc={mobileSrc} />;
+};
