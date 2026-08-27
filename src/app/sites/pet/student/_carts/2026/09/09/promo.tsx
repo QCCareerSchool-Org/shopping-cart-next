47b54b7e5c3d9372aba5ac88361c92fe487d2c32
Angela Li <angelayxli@gmail.com>
2026-08-27 11:31:29 -0400

    schedule promos

---
+++ src/app/sites/pet/student/_carts/2026/09/09/promo.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import type { FC } from 'react';
+
+import { Hero20260909 } from './hero';
+import { CountDownTimerWrapper } from '@/components/countDownTimer/countDownTimerWrapper';
+import { Section } from '@/components/section';
+import type { LastChancePeriodDTO } from '@/lib/period';
+
+const backgroundColor = '#615960';
+
+interface Props {
+  date: number;
+  period: LastChancePeriodDTO;
+}
+
+export const PetStudent20260909Promo: FC<Props> = ({ date, period }) => {
+
+  return (
+    <>
+      <Section style={{ backgroundColor }} noPadding>
+        <Hero20260909 />
+      </Section>
+      <CountDownTimerWrapper
+        date={date}
+        showDate={period.lastChance}
+        endDate={period.end}
+        message={<span style={{ textTransform: 'uppercase' }}>This special offer ends soon!</span>}
+        className="bg-primary text-light"
+      />
+    </>
+  );
+};
